e87f6bb188f3032a16bd83a8623fefeccc9fdaf7
baka-gourd <fanng233@gmail.com>
2020-03-16 08:49:05 +0800

    update

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+# coding=utf-8
 # import
 import env
 import linecache
+import json
 from env import instances_choose
+from urllib.request import Request
+from urllib.request import urlopen
 
 # var
 downloader_config = "./config/downloader.ini"
+
 # code
+
+# 选择实例
 print("您选择的实例名称是：" + instances_choose)
 using_path = linecache.getline('./config/instances/' + instances_choose, 2)
 using_version = linecache.getline('./config/instances/' + instances_choose, 3)
@@ -13,3 +20,15 @@ using_path = using_path[:-1]
 using_version = using_version[:-1]
 print(using_path)
 print(using_version)
+
+# 搜索
+search_key = str("craft")  # input("请输入搜索关键词：")
+page_count = str(1)
+
+request = Request('https://addons-ecs.forgesvc.net/api/v2/addon/search?categoryId=0&gameId=432&gameVersion=' +
+                  using_version+'&index='+page_count+'&pageSize=10&searchFilter='+search_key)
+
+response_body = urlopen(request).read()
+search_cache = open('./cache/search_cache.json', 'w')
+print(response_body, file=search_cache)
+search_cache.close()
