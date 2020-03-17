688e9d42b5b1f05a105a69743d17f63893e53bd6
baka-gourd <fanng233@gmail.com>
2020-03-17 19:21:27 +0800

    fix

--- env.py
+++ env.py
@@ -6,7 +6,9 @@ cfg = configparser.ConfigParser()
 instances_config = 'instances.ini'
 cache_dir = './cache'
 cfg.read(instances_config)
-
+default = ""
+instances_choose = ""
+instances_list = []
 
 # 检查文件
 if not os.path.exists(cache_dir):
@@ -19,24 +21,32 @@ if not os.path.exists('./downloader.ini'):
     init_downloader.close()
 
 print("正在加载...下载和搜索速度视网络环境而定。")
-default = input("是否使用已有实例[Y/N]：")
-if default == "y" or default == "Y":  # 选择配置
-    instances_list = cfg.sections()
-    for i in instances_list:
-        print(i)
-    instances_choose = input("请选择实例：")
-elif default == "n" or default == "N":  # 写入配置
-    instance_name = input("实例名称（不可相同！）：")
-    instance_path = input("实例（.minecraft）绝对路径：")
-    instance_version = input("实例版本：")
-    cfg.add_section(instance_name)
-    cfg.set(instance_name, "path", instance_path)
-    cfg.set(instance_name, "version", instance_version)
-    with open("./instances.ini", "w+") as icfg:
-        cfg.write(icfg)
-    instances_choose = instance_name
-else:
-    print("\033[1;31m 请重新检查输入!\033[0m")
+while default != "y" or default != "Y" or default != "n" or default != "N":
+    if instances_choose not in instances_list:
+        default = input("是否使用已有实例[Y/N]：")
+        if default == "y" or default == "Y":  # 选择配置
+            instances_list = cfg.sections()
+            for i in instances_list:
+                print(i)
+            instances_choose = input("请选择实例：")
+            while instances_choose not in instances_list:
+                print("\033[1;31m 请重新检查输入!\033[0m")
+                instances_choose = input("请选择实例：")
+        elif default == "n" or default == "N":  # 写入配置
+            instance_name = input("实例名称（不可相同！）：")
+            instance_path = input("实例（.minecraft）绝对路径：")
+            instance_version = input("实例版本：")
+            cfg.add_section(instance_name)
+            cfg.set(instance_name, "path", instance_path)
+            cfg.set(instance_name, "version", instance_version)
+            with open("./instances.ini", "w+") as icfg:
+                cfg.write(icfg)
+            instances_choose = instance_name
+        else:
+            print("\033[1;31m 请重新检查输入!\033[0m")
+    else:
+        break
+
 
 using_path = cfg.get(instances_choose, "path")
 using_version = cfg.get(instances_choose, "version")
